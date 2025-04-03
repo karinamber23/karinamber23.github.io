6c4ac94177370bc984840c26bc0830fb34a5a43e
nood-leog <116304896+nood-leog@users.noreply.github.com>
2025-04-03 15:28:19 +0000

    Deploying to gh-pages from @ nood-leog/nood-leog.github.io@e71d1cea9a41e10791797eb377bf264665b90e97 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "56128fc6ec7568b99d6fc24cfa34a218",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "74abef179755e939101394a07ee16624"};
+"flutter_bootstrap.js": "db3701cd98c0f0d2c0d3d5e0c58cc084"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
